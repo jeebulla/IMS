c5e72a89d97475e8d51da346a9b7861427d1a49f
Muhammad Najeeb <yusnaj21@gmail.com>
2023-08-27 01:22:05 +0100

    Initialized navbar and hero sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./dist/*.{html,js}"],
+  darkMode: "class",
   theme: {
     extend: {},
+    colors: {
+      darkBlue: "#141E46",
+      darkRed: "#BB2525",
+      light: "#FFF5E0",
+      lightRed: "#FF6969",
+    },
   },
   plugins: [],
 };
